ccd714a782d8255eab3772d04ab71d9b623518a2
ducc1998 <duccoc98@gmail.com>
2021-01-28 15:41:50 +0700

    register

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    function register(Request $request)
+    {
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+    }
+
+    function showFormRes()
+    {
+        return view('register');
+    }
+}
