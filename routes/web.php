d4d86dfee1e2815ea173a7945cf4a016cf6d5e3b
lam132409 <lam132409329@gmail.com>
2021-01-28 10:48:45 +0700

    aksdjfkj:

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/listhouse', function () {
+    return view('propertyHouse');
+});
